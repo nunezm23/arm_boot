846d6e4d5eb5848d0e1ff00366257925b3b8de2b
Marcos Nunez <mnnunezc23@gmail.com>
2025-09-29 22:53:17 -0500

    Refactor startup for stm32l476rg

--- myprogram.c
+++ myprogram.c
@@ -2,25 +2,3 @@ int main(void)
 {
     return 0;
 }
-
-extern const uint32_t _stored_data;   // LMA (in FLASH)
-extern uint32_t _start_data;          // VMA (in RAM)
-extern uint32_t _end_data;
-
-void init_data(void) {
-    uint32_t *src = (uint32_t*)&_stored_data;
-    uint32_t *dst = (uint32_t*)&_start_data;
-    while (dst < (uint32_t*)&_end_data) {
-        *dst++ = *src++;
-    }
-}
-
-extern uint32_t _start_bss;
-extern uint32_t _end_bss;
-
-void zero_bss(void) {
-    uint32_t *p = (uint32_t*)&_start_bss;
-    while (p < (uint32_t*)&_end_bss) {
-        *p++ = 0;
-    }
-}
--- startup_stml476rg.c
+++ startup_stml476rg.c
@@ -1,24 +1,159 @@
-extern int main(void);
-
 /**
- * @brief Basic implementation for isr_reset function. Here I should to copy or initialize .data and .bss section
+ * @author: Marcos Nunez
+ * @date: 29/09/205
+ * @version: 1.0.0
  * 
  */
-void isr_reset(void)
+
+extern unsigned int _stored_data;
+extern unsigned int _start_data;
+extern unsigned int _end_data;
+extern unsigned int _start_bss;
+extern unsigned int _end_bss;
+
+extern void *END_STACK;
+
+static int zeroed_variable_in_bss;
+static int initialized_variable_in_data = 42;
+
+/**
+ * @brief Main program loop
+ * 
+ */
+void main(void) {
+    while(1) {
+        zeroed_variable_in_bss++;
+        initialized_variable_in_data++;
+    }
+}
+
+void isr_fault(void)
 {
+    while(1) ;;
+}
+
+/**
+ * @brief Dummy ISR.
+ * 
+ */
+void isr_empty(void)
+{
+    /** Nothing here. */
+}
+
+/**
+ * @brief Bus fault handler.
+ * 
+ */
+void isr_bus_fault(void)
+{
+    while(1);
+}
+
+/**
+ * @brief UART0 interrupt handler
+ * 
+ */
+void isr_uart0(void)
+{
+    while(1);
+}
+
+/**
+ * @brief Ethernet interrupt handler
+ * 
+ */
+void isr_ethernet(void)
+{
+    while(1);
+}
+
+/**
+ * @brief Non-Maskable interrupt
+ * 
+ */
+void isr_nmi(void)
+{
+    while(1);
+}
+
+/**
+ * @brief Hard fault handler
+ * 
+ */
+void isr_hard_fault(void)
+{
+    while(1);
+}
+
+/**
+ * @brief Memory fault handler
+ * 
+ */
+void isr_mem_fault(void)
+{
+    while(1);
+}
+
+/**
+ * @brief Usage interrupt handler
+ * 
+ */
+void isr_usage_fault(void)
+{
+    while(1);
+}
+
+/**
+ * @brief Interrupt reset handler
+ * @warning Here just copy configure .data and .bss section and then laun main?
+ * 
+ */
+void isr_reset(void) {
+    unsigned int *src, *dst;
+
+    /* Copy the .data section from flash to RAM. */
+    src = (unsigned int *) &_stored_data;
+    dst = (unsigned int *) &_start_data;
+    while (dst < (unsigned int *)&_end_data) {
+        *dst = *src;
+        dst++;
+        src++;
+    }
+
+    /** Here the BSS section is initialized with zeros. */
+    dst = &_start_bss;
+    while (dst < (unsigned int *)&_end_bss) {
+        *dst = 0U;
+        dst++;
+    }
+
     main();
-    while(1);
 }
 
-void isr_hard_fault()
-{
-    while(1);
-}
 
-#define IVT_ARRAY_SIZE (48)
-
-typedef void (*isr_t)(void);
-static const isr_t ivt[IVT_ARRAY_SIZE] = 
+/**
+ * @brief Interrupt Vector Table
+ * 
+ */
+__attribute__ ((section(".isr_vector")))
+void (* const IV[])(void) =
 {
-    (isr_t)&_stack,
-}:
+    (void (*)(void))(&END_STACK),
+    isr_reset,          /** ISR reset handler. */
+    isr_nmi,            /** Non-Maskable Interrupt (NMI) */
+    isr_hard_fault,     /** Hard fault. */
+    isr_mem_fault,      /** Memory exception. */
+    isr_bus_fault,      /** Bus fault. */
+    isr_usage_fault,    /** Usage fault. */
+    0, 0, 0, 0,         /** Reserved x4. */
+    isr_empty,          /** Supervisor call. */
+    isr_empty,          /** Debug monitor event. */
+    0,                  /** Reserved. */
+    isr_empty,          /** PendSV call. */
+    isr_empty,          /** System tick. */
+    /** Interrupts for external peripherals as:  */
+    isr_uart0,          /** Uart 0 */
+    isr_ethernet,       /** Ethernet ISR */
+    /** ... many more external interrupts follow */
+};
